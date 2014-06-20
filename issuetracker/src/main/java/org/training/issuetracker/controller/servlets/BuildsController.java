e9bbf39366946db8772b68066d6b8f024c783206
Yury Kiryla <yurykiryla@gmail.com>
2014-06-20 17:03:20 +0300

    0.3.3

---
+++ issuetracker/src/main/java/org/training/issuetracker/controller/servlets/BuildsController.java
@@ -0,0 +1,41 @@
+package org.training.issuetracker.controller.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.training.issuetracker.controller.AbstractController;
+import org.training.issuetracker.controller.Constants;
+import org.training.issuetracker.model.bean.Build;
+import org.training.issuetracker.model.bean.Project;
+import org.training.issuetracker.model.dao.exception.DAOException;
+import org.training.issuetracker.model.dao.factory.DAOFactory;
+
+import com.google.gson.Gson;
+
+
+public class BuildsController extends AbstractController {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void performTask(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		int id = getId(request, Constants.KEY_PROJECT);
+		try {
+			Project project = DAOFactory.getProjectDAO().getOb(id);
+			Map<Integer, String> builds = new HashMap<>();
+			for (Build build : project.getBuilds()) {
+				builds.put(build.getId(), build.getName());
+			}
+			response.setContentType("application/json");
+			response.getWriter().write(new Gson().toJson(builds));
+		} catch (DAOException e) {
+			errorForward(e, request, response);
+		}
+		
+	}
+}
