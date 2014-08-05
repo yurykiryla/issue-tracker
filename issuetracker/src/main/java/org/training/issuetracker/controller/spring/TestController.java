0801d979c23ca227b3e77be67fd7c2a657f96052
Yury Kiryla <yurykiryla@gmail.com>
2014-08-05 09:43:50 -0430

    0.4.3

---
+++ issuetracker/src/main/java/org/training/issuetracker/controller/spring/TestController.java
@@ -0,0 +1,17 @@
+package org.training.issuetracker.controller.spring;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/")
+	public String temp(Map<String, Object> map) {
+		map.put("test", "testing");
+		
+		return "main";
+	}
+}
